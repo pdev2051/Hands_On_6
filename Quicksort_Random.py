4d27858602da00aeb25a740dd46a7e7142078600
pdev2051 <69709704+pdev2051@users.noreply.github.com>
2025-02-25 20:08:38 -0600

    Add files via upload

---
+++ Quicksort_Random.py
@@ -0,0 +1,18 @@
+import random
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot_index = random.randint(0, len(arr) - 1)
+    pivot = arr[pivot_index]
+    
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    return quicksort(left) + middle + quicksort(right)
+
+arr = [3, 6, 8, 10, 1, 2, 1]
+sorted_arr = quicksort(arr)
+print(sorted_arr)
